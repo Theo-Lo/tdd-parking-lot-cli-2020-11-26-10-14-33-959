8f04b2ad04e12ad1ff25ddad39a0f2844322c9d3
Theo Lo <theo.lo@oocl.com>
2020-11-27 16:02:53 +0800

    [ParkingLot][Theo]refactor: used hashmap instead of using license for checking

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -2,36 +2,47 @@ package com.oocl.cultivation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ParkingLot {
     private final Integer capacity;
     private List<Car> cars;
+    private final Map<Ticket, Car> carTicketHashMap;
 
     public ParkingLot(Integer capacity){
         this.capacity = capacity;
-        this.cars = new ArrayList<>();
+//        this.cars = new ArrayList<>();
+        this.carTicketHashMap = new HashMap<>();
     }
 
     public Ticket park(Car car){
-        if(this.capacity - cars.size() <= 0){
+//        if(capacity - this.cars.size() == 0){
+//            return null;
+//        }
+        if(this.carTicketHashMap.size() >= capacity){
             return null;
         }
-        final Ticket ticket = new Ticket(car.getLicense());
-        cars.add(car);
-        return new Ticket(ticket.getLicense());
+        Ticket ticket = new Ticket(car.getLicense());
+//        cars.add(car);
+        this.carTicketHashMap.put(ticket,car);
+        return ticket;
     }
 
     public Car fetch(Ticket ticket) {
-        if(ticket.getUsed()){
-            return null;
-        }
-        for(Car carList:cars){
-            if(carList.getLicense().equals(ticket.getLicense())){
-                ticket.setUsed();
-                cars.remove(carList);
-                return carList;
-            }
-        }
-        return null;
+        Car car = this.carTicketHashMap.get(ticket);
+        this.carTicketHashMap.remove(ticket);
+        return car;
+//        if(ticket.getUsed()){
+//            return null;
+//        }
+//        for(Car carList:cars){
+//            if(carList.getLicense().equals(ticket.getLicense())){
+//                ticket.setUsed();
+//                cars.remove(carList);
+//                return carList;
+//            }
+//        }
+//        return null;
     }
 }
