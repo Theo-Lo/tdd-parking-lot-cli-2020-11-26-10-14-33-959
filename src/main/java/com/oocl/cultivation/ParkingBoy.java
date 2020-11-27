40e0df4bd45e5c20580f7ee60ca387a3e6d4ca7a
Theo Lo <theo.lo@oocl.com>
2020-11-27 18:19:57 +0800

    [ParkingLot][Theo]test: create unit test for returning parking ticker for parking 2 cars given parking lot 1 is full

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -3,15 +3,40 @@ package com.oocl.cultivation;
 import com.oocl.cultivation.exceptions.NotEnoughPositionException;
 import com.oocl.cultivation.exceptions.UnrecognizedParkingTicketException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingBoy {
-    private final ParkingLot parkingLot;
+    ParkingLot parkingLot;
+    List<ParkingLot> parkingLotList = new ArrayList<>();
     public ParkingBoy(ParkingLot parkingLot){
         this.parkingLot = parkingLot;
+        this.parkingLotList.add(parkingLot);
     }
+
+    public ParkingBoy(List<ParkingLot> parkingLotList) {
+        this.parkingLotList = parkingLotList;
+    }
+
     public Ticket park(Car car) throws NotEnoughPositionException {
-        return this.parkingLot.park(car);
+        for (ParkingLot parkingLot : parkingLotList) {
+            try {
+                return parkingLot.park(car);
+            } catch (NotEnoughPositionException notEnoughPositionException) {
+                // Do nothing
+            }
+        }
+        throw new NotEnoughPositionException();
     }
+
     public Car fetch(Ticket ticket) throws UnrecognizedParkingTicketException {
-        return parkingLot.fetch(ticket);
+        for (ParkingLot parkingLot : parkingLotList) {
+            try {
+                return parkingLot.fetch(ticket);
+            } catch (UnrecognizedParkingTicketException unrecognizedParkingTicketException) {
+                // Do nothing
+            }
+        }
+        throw new UnrecognizedParkingTicketException();
     }
 }
--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -5,6 +5,9 @@ import com.oocl.cultivation.exceptions.UnrecognizedParkingTicketException;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -14,13 +17,14 @@ class ParkingBoyTest {
     void should_parking_boy_call_parking_lot_park_function_once_when_park_the_car() throws NotEnoughPositionException {
         //given
         ParkingLot parkingLot = Mockito.mock(ParkingLot.class);
-        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
         Car car = new Car();
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        parkingLotList.add(parkingLot);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotList);
         //when
         parkingBoy.park(car);
         //then
         verify(parkingLot, times(1)).park(car);
-
     }
 
     @Test
@@ -56,5 +60,24 @@ class ParkingBoyTest {
             parkingLot.fetch(new Ticket());
         });
         assertEquals("Unrecognized parking ticket", UnrecognizedParkingTicketException.getMessage());
+    }
+
+    @Test
+    void should_return_parking_ticket_for_parking_2_given_parking_lot_1_is_full() throws NotEnoughPositionException, UnrecognizedParkingTicketException {
+        //given
+        Car car = new Car();
+        Car car2 = new Car();
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot(10);
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        parkingLotList.add(parkingLot1);
+        parkingLotList.add(parkingLot2);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotList);
+        parkingBoy.park(car);
+        //when
+        Ticket ticket2 = parkingBoy.park(car2);
+        //then
+        assertNotNull(ticket2);
+        assertEquals(car2, parkingLot2.fetch(ticket2));
     }
 }
