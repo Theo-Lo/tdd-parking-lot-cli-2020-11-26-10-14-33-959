4195d418d00fe3216541fc2ad818b56eba99ff56
Theo Lo <theo.lo@oocl.com>
2020-11-27 15:40:01 +0800

    [ParkingLot][Theo]test: create unit test to return parking ticket when park car given a car and a parking lot with available capacity

--- src/test/java/com/oocl/cultivation/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/ParkingLotTest.java
@@ -5,5 +5,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLotTest {
+    @Test
+    void should_return_a_parking_ticket_when_park_the_car_given_a_car_and_parking_lot_with_available_capacity(){
+        //given
+        Car car = new Car("car1");
+        ParkingLot parkingLot = new ParkingLot(10);
 
+        //when
+        final Ticket ticket = parkingLot.park(car);
+
+        //then
+        assertNotNull(ticket);
+    }
 }
