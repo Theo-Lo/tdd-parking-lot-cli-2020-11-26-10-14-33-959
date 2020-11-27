e0d1e4792b4def3b7f151e723b9a390bfc9d11da
Theo Lo <theo.lo@oocl.com>
2020-11-27 20:49:35 +0800

    [ParkingLotServiceManager][Theo]test: create unit test to return a ticket when assign parking boy to park a car given he is assigned to that parking lot, last commit is feature

--- src/test/java/com/oocl/cultivation/ParkingLotServiceManagerTest.java
+++ src/test/java/com/oocl/cultivation/ParkingLotServiceManagerTest.java
@@ -32,4 +32,24 @@ public class ParkingLotServiceManagerTest {
         assertTrue(serviceManager.getParkingBoysList().contains(smartParkingBoy));
         assertTrue(serviceManager.getParkingBoysList().contains(superSmartParkingBoy));
     }
+
+    @Test
+    void should_return_a_ticket_when_assign_parking_boy_to_park_a_car_given_he_is_assigned_to_that_parking_lot(){
+        //given
+        Car car = new Car();
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        ParkingLot parkingLot = new ParkingLot(10);
+        parkingLotList.add(parkingLot);
+
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotList);
+        HashSet<ParkingBoy> parkingBoyList = new HashSet<>();
+        parkingBoyList.add(parkingBoy);
+        ParkingLotServiceManager serviceManager = new ParkingLotServiceManager(new ArrayList<>(), parkingBoyList);
+
+        //when
+        Ticket ticket = serviceManager.assignParkingBoyToPark(parkingBoy, car);
+
+        //then
+        assertNotNull(ticket);
+    }
 }
