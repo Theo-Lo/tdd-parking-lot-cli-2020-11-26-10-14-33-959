a08971761b60f6666c8b4b4086f776a0eddbff06
Theo Lo <theo.lo@oocl.com>
2020-11-27 15:40:41 +0800

    [ParkingLot][Theo]test: create unit test to return only one car parked when park multiple car and parking lot with 1 capacity

--- src/test/java/com/oocl/cultivation/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/ParkingLotTest.java
@@ -17,4 +17,20 @@ public class ParkingLotTest {
         //then
         assertNotNull(ticket);
     }
+
+    @Test
+    void should_only_one_car_parked_when_park_multiple_cars_given_multiple_car_and_parking_lot_with_1_capacity(){
+        //given
+        Car car1 = new Car("car1");
+        Car car2 = new Car("car2");
+        ParkingLot parkingLot = new ParkingLot(1);
+
+        //when
+        final Ticket ticket1 = parkingLot.park(car1);
+        final Ticket ticket2 = parkingLot.park(car2);
+
+        //then
+        assertNotNull(ticket1);
+        assertNull(ticket2);
+    }
 }
