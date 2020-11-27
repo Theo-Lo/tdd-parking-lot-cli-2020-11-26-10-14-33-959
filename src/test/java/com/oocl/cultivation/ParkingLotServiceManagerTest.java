6d682c64fd2f65958d51173f45b02cde1f54e6a1
Theo Lo <theo.lo@oocl.com>
2020-11-27 21:14:26 +0800

    [ParkingLotServiceManager][Theo]test: create unit test to throw parking bot not in management list when assign parking boy to fetch a car given he is not in management list

--- src/test/java/com/oocl/cultivation/ParkingLotServiceManagerTest.java
+++ src/test/java/com/oocl/cultivation/ParkingLotServiceManagerTest.java
@@ -94,4 +94,27 @@ public class ParkingLotServiceManagerTest {
         assertEquals(car, serviceManager.assignParkingBoyToFetch(parkingBoy, ticket));
     }
 
+    @Test
+    void should_throw_parking_boy_not_in_management_list_when_assign_parking_boy_to_fetch_a_car_given_he_is_not_in_management_list() throws Exception {
+        //given
+        Car car = new Car();
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        ParkingLot parkingLot = new ParkingLot(10);
+        parkingLotList.add(parkingLot);
+
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotList);
+        ParkingBoy parkingBoy2 = new ParkingBoy(new ArrayList<>());
+        HashSet<ParkingBoy> parkingBoyList = new HashSet<>();
+        parkingBoyList.add(parkingBoy);
+        ParkingLotServiceManager serviceManager = new ParkingLotServiceManager(new ArrayList<>(), parkingBoyList);
+        Ticket ticket = serviceManager.assignParkingBoyToPark(parkingBoy, car);
+
+        //when
+        final ParkingBoyNotInManagementListException parkingBoyNotInManagementListException = assertThrows(ParkingBoyNotInManagementListException.class, () -> {
+            serviceManager.assignParkingBoyToFetch(parkingBoy2, ticket);
+        });
+        //then
+        assertEquals("ParkingBoy not in management list", parkingBoyNotInManagementListException.getMessage());
+    }
+
 }
